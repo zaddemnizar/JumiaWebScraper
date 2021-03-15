b2c6e30789f87d7994d973a7c1e743e6cb81ce60
Nizar <79638878+zaddemnizar@users.noreply.github.com>
2021-03-15 23:37:34 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,8 +3,6 @@ using HtmlAgilityPack;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-using System.Linq;
 
 namespace JumiaWebScraper
 {
@@ -14,93 +12,53 @@ namespace JumiaWebScraper
         {
             string webUrl = "https://www.jumia.com.tn";
             HtmlWeb web = new HtmlWeb();
-
-            HtmlDocument doc = web.Load(webUrl+ "/peripheriques-logiciels-accessoires/");
-
-            List<string> hrefArticles = new List<string>();
-
-            foreach (var item in doc.DocumentNode.SelectNodes("//*[@class=\"core\"]/@href"))
-            {
-                hrefArticles.Add(item.Attributes["href"].Value);
-            }
-
-            var nbArticlesText = doc.DocumentNode.SelectNodes("//p [@class=\"-gy5 -phs\"]");
+            HtmlDocument doc = web.Load(webUrl + "/son-informatique/");
             
-            int nbArticles = Int32.Parse(nbArticlesText[0].InnerText.Substring(0, nbArticlesText[0].InnerText.IndexOf(" ")));
+            //extract href articles 1 ere page
+            List<string> hrefArticles = new List<string>();
+            foreach (var item in doc.DocumentNode.SelectNodes("//*[@class=\"core\"]/@href"))
+                hrefArticles.Add(item.Attributes["href"].Value);
 
-            int nbPages = nbArticles / 40;
-            if (nbPages > 50) nbPages = 50;
+            //nombre de pages à parcourir
+            var nbArticlesText = doc.DocumentNode.SelectSingleNode("//p [@class=\"-gy5 -phs\"]");
+            int nbArticles = Int32.Parse(nbArticlesText.InnerText.Substring(0, nbArticlesText.InnerText.IndexOf(" ")));
+            int nbPages = (int)(((nbArticles / 40) > 50) ? 50 : Math.Round(Convert.ToDecimal(nbArticles / 40))+1);
 
-            for (int i = 0; i < nbPages-1; i++)
+            for (int i = 0; i < nbPages - 1; i++)
             {
-                var nextPage = doc.DocumentNode.SelectNodes("//a[@aria-label=\"Page suivante\"]").ToList();
-
-                //Console.WriteLine(nextPage[0].Attributes["href"].Value);
-                Console.WriteLine("charging page "+(i+2).ToString()+" / "+nbPages.ToString());
-
-                string link = webUrl + nextPage[0].Attributes["href"].Value;
+                Console.WriteLine($"charging page{i + 2}/ {nbPages}");
+                string link = webUrl + doc.DocumentNode.SelectSingleNode("//a[@aria-label=\"Page suivante\"]").Attributes["href"].Value;
                 doc = web.Load(link);
                 foreach (var item in doc.DocumentNode.SelectNodes("//*[@class=\"core\"]/@href"))
                 {
                     hrefArticles.Add(item.Attributes["href"].Value);
                 }
             }
-            
-            List<string> nomArticles = new List<string>();
-            List<string> prixArticles = new List<string>();
-            List<string> skuArticles = new List<string>();
-            List<string> couleurArticles = new List<string>();
-            List<string> modelArticles = new List<string>();
-            List<string> poidsArticles = new List<string>();
-            List<string> finalArticles = new List<string>();
-            decimal prog = 0;
 
-            for (int i = 0; i < /*hrefArticles.Count -*/10; i++)
+            //Parcourir tous les articles
+            List<string> finalArticles = new List<string> { "Nom;Prix;SKU;Couleur;Model;Poids" };
+            for (int i = 0; i < hrefArticles.Count -1; i++)
             {
-                Console.WriteLine($"Progress {i/10}%");
+                Console.WriteLine($"Progress {Math.Round((Convert.ToDecimal(i) / hrefArticles.Count)*100,2)} %");
                 string articleLink = webUrl + hrefArticles[i];
                 doc = web.Load(articleLink);
 
                 var nom = doc.DocumentNode.SelectSingleNode("//h1/@class")?.InnerText;
                 var prix = doc.DocumentNode.SelectSingleNode("//span[@dir=\"ltr\"]")?.InnerText;
+                var sku = doc.DocumentNode.SelectSingleNode("//*[text()=\"SKU\"]//following-sibling::text()")?.InnerText.Substring(2);
+                var couleur = doc.DocumentNode.SelectSingleNode("//*[text()=\"Couleur\"]//following-sibling::text()")?.InnerText.Substring(2);
+                var modele = doc.DocumentNode.SelectSingleNode("//*[text()=\"Modèle\"]//following-sibling::text()")?.InnerText.Substring(2);
+                var poids = doc.DocumentNode.SelectSingleNode("//*[text()=\"Poids (kg)\"]//following-sibling::text()")?.InnerText.Substring(2);
 
-                //foreach (var item in doc.DocumentNode.SelectNodes("//span[@dir=\"ltr\"]"))
-                //{
-                //    prixarticles.add(item.innertext.substring(0, item.innertext.indexof(" ")));
-                //    break;
-                //}
-                //foreach (var item in doc.DocumentNode.SelectNodes("//article[3]/div/ul/li[1]/text()"))
-                //{
-                //    skuArticles.Add(item.InnerText.Substring(2));
-                //    break;
-                //}
-                //foreach (var item in doc.DocumentNode.SelectNodes("//article[3]/div/ul/li[2]/text()"))
-                //{
-                //    couleurArticles.Add(item.InnerText.Substring(2));
-                //    break;
-                //}
-                //foreach (var item in doc.DocumentNode.SelectNodes("//article[3]/div/ul/li[3]/text()"))
-                //{
-                //    modelArticles.Add(item.InnerText.Substring(2));
-                //    break;
-                //}
-                //foreach (var item in doc.DocumentNode.SelectNodes("//article[3]/div/ul/li[4]/text()"))
-                //{
-                //    poidsArticles.Add(item.InnerText.Substring(2));
-                //    break;
-                //}
-
-                //finalArticles.Add(string.Concat(nomArticles[i],";",prixArticles[i], ";", skuArticles[i], ";", couleurArticles[i], ";", modelArticles[i], ";", poidsArticles[i]));
-
-                finalArticles.Add(string.Concat(nom,";",prix));
+                finalArticles.Add(string.Concat(nom, ",", prix, ",", sku, ",", couleur, ",", modele, ",", poids));
             }
 
             foreach (string article in finalArticles)
                 Console.WriteLine(article);
 
-            Console.WriteLine(hrefArticles.Count+"  articles");
-            Console.WriteLine(nbArticles + "  articles");
-            
+            Console.WriteLine(hrefArticles.Count + "  articles");
+
+            //creation fichier csv
             StringBuilder sb = new StringBuilder();
             foreach (var article in finalArticles)
             {
@@ -112,4 +70,3 @@ namespace JumiaWebScraper
         }
     }
 }
- 
