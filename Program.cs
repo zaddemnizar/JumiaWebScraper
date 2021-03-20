f39340ace93e5a49050fc3ee23c6734bd8818fba
Nizar <79638878+zaddemnizar@users.noreply.github.com>
2021-03-20 18:18:11 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,34 +1,37 @@
 using System;
-using HtmlAgilityPack;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Text;
+using HtmlAgilityPack;
 
-namespace JumiaWebScraper
+namespace jumiascraping
 {
     class Program
     {
         static void Main(string[] args)
         {
+            WebClient client = new WebClient();
             string webUrl = "https://www.jumia.com.tn";
-            HtmlWeb web = new HtmlWeb();
-            HtmlDocument doc = web.Load(webUrl + "/son-informatique/");
+            string webCode = client.DownloadString(webUrl + "/son-informatique/");
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(webCode);
             
-            //extract href articles 1 ere page
+            //extract liens articles 1ere page
             List<string> hrefArticles = new List<string>();
             foreach (var item in doc.DocumentNode.SelectNodes("//*[@class=\"core\"]/@href"))
                 hrefArticles.Add(item.Attributes["href"].Value);
-
+            
             //nombre de pages à parcourir
             var nbArticlesText = doc.DocumentNode.SelectSingleNode("//p [@class=\"-gy5 -phs\"]");
             int nbArticles = Int32.Parse(nbArticlesText.InnerText.Substring(0, nbArticlesText.InnerText.IndexOf(" ")));
-            int nbPages = (int)(((nbArticles / 40) > 50) ? 50 : Math.Round(Convert.ToDecimal(nbArticles / 40))+1);
+            int nbPages = (int)(((nbArticles / 40) > 50) ? 50 : Math.Round(Convert.ToDecimal(nbArticles / 40)) + 1);
 
             for (int i = 0; i < nbPages - 1; i++)
             {
-                Console.WriteLine($"charging page{i + 2}/ {nbPages}");
-                string link = webUrl + doc.DocumentNode.SelectSingleNode("//a[@aria-label=\"Page suivante\"]").Attributes["href"].Value;
-                doc = web.Load(link);
+                Console.WriteLine($"charging page {i + 2}/ {nbPages}");
+                string link = client.DownloadString(webUrl + doc.DocumentNode.SelectSingleNode("//a[@aria-label=\"Page suivante\"]").Attributes["href"].Value);
+                doc.LoadHtml(link);
                 foreach (var item in doc.DocumentNode.SelectNodes("//*[@class=\"core\"]/@href"))
                 {
                     hrefArticles.Add(item.Attributes["href"].Value);
@@ -37,18 +40,22 @@ namespace JumiaWebScraper
 
             //Parcourir tous les articles
             List<string> finalArticles = new List<string> { "Nom;Prix;SKU;Couleur;Model;Poids" };
-            for (int i = 0; i < hrefArticles.Count -1; i++)
+            for (int i = 0; i < hrefArticles.Count - 1; i++)
             {
-                Console.WriteLine($"Progress {Math.Round((Convert.ToDecimal(i) / hrefArticles.Count)*100,2)} %");
-                string articleLink = webUrl + hrefArticles[i];
-                doc = web.Load(articleLink);
+                Console.WriteLine($"Progress {Math.Round((Convert.ToDecimal(i) / hrefArticles.Count) * 100, 2)} %");
+                string articleLink = client.DownloadString(webUrl + hrefArticles[i]);
+                doc.LoadHtml(articleLink);
 
                 var nom = doc.DocumentNode.SelectSingleNode("//h1/@class")?.InnerText;
-                var prix = doc.DocumentNode.SelectSingleNode("//span[@dir=\"ltr\"]")?.InnerText;
+                var prix = doc.DocumentNode.SelectSingleNode("//span[@dir=\"ltr\"]")?.InnerText.Replace(";", " ");
                 var sku = doc.DocumentNode.SelectSingleNode("//*[text()=\"SKU\"]//following-sibling::text()")?.InnerText.Substring(2);
                 var couleur = doc.DocumentNode.SelectSingleNode("//*[text()=\"Couleur\"]//following-sibling::text()")?.InnerText.Substring(2);
                 var modele = doc.DocumentNode.SelectSingleNode("//*[text()=\"Modèle\"]//following-sibling::text()")?.InnerText.Substring(2);
                 var poids = doc.DocumentNode.SelectSingleNode("//*[text()=\"Poids (kg)\"]//following-sibling::text()")?.InnerText.Substring(2);
+
+                //télécharger image correspondante
+                string imageurl = doc.DocumentNode.SelectSingleNode("//img[@class=\"-fw -fh\"]").Attributes["data-src"].Value;
+                client.DownloadFile(imageurl, sku+ ".jpg");
 
                 finalArticles.Add(string.Concat(nom, ";", prix, ";", sku, ";", couleur, ";", modele, ";", poids));
             }
